031599ec800399d72feab8ccf57400d82a7c036b
Martin Perussini <mperussini@frsf.utn.edu.ar>
2021-05-12 17:48:28 -0300

    ClienteRepository Override de metodos Los metodos findAll y findById se sobreescribieron para que solo traigan los que no tienen fechaBaja

--- src/main/java/dan/tp2021/danmsusuarios/dao/ClienteRepository.java
+++ src/main/java/dan/tp2021/danmsusuarios/dao/ClienteRepository.java
@@ -4,6 +4,10 @@ import dan.tp2021.danmsusuarios.domain.Cliente;
 import frsf.isi.dan.InMemoryRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Repository
 public class ClienteRepository extends InMemoryRepository<Cliente> {
     @Override
@@ -13,5 +17,19 @@ public class ClienteRepository extends InMemoryRepository<Cliente> {
     @Override
     public void setId(Cliente entity, Integer id) {
         entity.setId(id);
+    }
+
+    @Override
+    public List<Cliente> findAll(){
+        return this.findAll().stream().filter(cliente -> cliente.getFechaBaja().equals(null)).collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<Cliente> findById(Integer id){
+        Optional<Cliente> optionalCliente = this.findById(id);
+        if(optionalCliente.get().getFechaBaja()==null){
+            return optionalCliente;
+        }
+        return null;
     }
 }
