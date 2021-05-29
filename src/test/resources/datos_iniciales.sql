62876069601af105deafc30488c95a2ea72bfd73
Gaston <gaston.diaz.contractor@macmillan.com>
2021-05-29 19:09:43 -0300

    Archivos de testing

---
+++ src/test/resources/datos_iniciales.sql
@@ -0,0 +1,4 @@
+create schema `dan-ms-usuarios`;
+
+insert into `dan-ms-usuarios`.tipo_usuario (id, tipo) values (1,'Tipo Usuario 1');
+insert into `dan-ms-usuarios`.tipo_usuario (id, tipo) values (2,'Tipo Usuario 2');
